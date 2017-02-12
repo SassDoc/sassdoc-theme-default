fbcdda761ed0395f2d473a2e6a96353d813f6b30
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-02-12 19:15:45 +0100

    Linted whole codebase with standard

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import chroma from 'chroma-js';
-import def from '../default';
-import { Promise } from 'es6-promise';
-import es6denodeify from 'es6-denodeify';
-import extend  from 'extend';
-import fs from 'fs-extra';
-import { minify } from 'html-minifier';
-import path from 'path';
-import sassdocExtras from 'sassdoc-extras';
-import swig from './swig';
+import def from '../default'
+import { Promise } from 'es6-promise'
+import es6denodeify from 'es6-denodeify'
+import extend from 'extend'
+import fs from 'fs-extra'
+import { minify } from 'html-minifier'
+import path from 'path'
+import sassdocExtras from 'sassdoc-extras'
+import swig from './swig'
 
-const denodeify = es6denodeify(Promise);
+const denodeify = es6denodeify(Promise)
 
-const copy = denodeify(fs.copy);
-const renderFile = denodeify(swig.renderFile);
-const writeFile = denodeify(fs.writeFile);
+const copy = denodeify(fs.copy)
+const renderFile = denodeify(swig.renderFile)
+const writeFile = denodeify(fs.writeFile)
 
 const applyDefaults = ctx =>
   extend({}, def, ctx, {
     groups: extend(def.groups, ctx.groups),
-    display: extend(def.display, ctx.display),
-  });
+    display: extend(def.display, ctx.display)
+  })
 
 const shortcutIcon = (dest, ctx) => {
   if (!ctx.shortcutIcon) {
-    ctx.shortcutIcon = { type: 'internal', url: 'assets/images/favicon.png' };
+    ctx.shortcutIcon = { type: 'internal', url: 'assets/images/favicon.png' }
   } else if (ctx.shortcutIcon.type === 'internal') {
-    ctx.shortcutIcon.url = 'assets/images/' + ctx.shortcutIcon.url;
+    ctx.shortcutIcon.url = 'assets/images/' + ctx.shortcutIcon.url
 
     return () =>
-      copy(ctx.shortcutIcon.path, path.resolve(dest, ctx.shortcutIcon.url));
+      copy(ctx.shortcutIcon.path, path.resolve(dest, ctx.shortcutIcon.url))
   }
-};
+}
 
 export default (dest, ctx) => {
-  ctx = applyDefaults(ctx);
+  ctx = applyDefaults(ctx)
   sassdocExtras(ctx,
     'description',
     'markdown',
@@ -42,10 +41,10 @@ export default (dest, ctx) => {
     'shortcutIcon',
     'sort',
     'resolveVariables'
-  );
-  ctx.data.byGroupAndType = sassdocExtras.byGroupAndType(ctx.data);
+  )
+  ctx.data.byGroupAndType = sassdocExtras.byGroupAndType(ctx.data)
 
-  const index = path.resolve(__dirname, '../views/documentation/index.html.swig');
+  const index = path.resolve(__dirname, '../views/documentation/index.html.swig')
 
   return Promise.all([
     copy(path.resolve(__dirname, '../assets'), path.resolve(dest, 'assets'))
@@ -53,6 +52,6 @@ export default (dest, ctx) => {
 
     renderFile(index, ctx)
       .then(html => minify(html, { collapseWhitespace: true }))
-      .then(html => writeFile(path.resolve(dest, 'index.html'), html)),
-  ]);
-};
+      .then(html => writeFile(path.resolve(dest, 'index.html'), html))
+  ])
+}
