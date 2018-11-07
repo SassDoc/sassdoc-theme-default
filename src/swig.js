cc9c15f4c11c7ac414ffd91289d3762e493a2398
René Roth <mail@reneroth.org>
2018-11-07 23:04:00 +0100

    Unescape name attribute

--- src/swig.js
+++ src/swig.js
@@ -46,8 +46,11 @@ const colorToHex = color =>
 const pluralize = input =>
   input.toLowerCase()
     .substring(input.length - 1) === 's'
-      ? input
-      : input + 's'
+    ? input
+    : input + 's'
+
+const unescape = input =>
+  input.replace(/\\/g, '')
 
 /**
  * Normalises a CSS color, then uses the YIQ algorithm to get the
@@ -67,3 +70,4 @@ swig.setFilter('is_color', isColor)
 swig.setFilter('display_as_type', safe(displayAsType))
 swig.setFilter('yiq', maybeYiqContrast)
 swig.setFilter('pluralize', pluralize)
+swig.setFilter('unescape', unescape)
